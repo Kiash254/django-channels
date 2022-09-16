c9fbc5d0bfed0e4b70f7893da85dc7e20092debc
Kiash254 <codewithkiash@gmail.com>
2022-09-16 22:03:50 +0300

    added chanells,decorators file and signals

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chats'
+    'chats',
+    'channels'
 ]
 
 MIDDLEWARE = [
